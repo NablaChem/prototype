a5f1a28e0dc67f5c9423f5b910763e5b908b7a60
von Rudorff, Guido Falk <guido@vonrudorff.de>
2024-10-17 18:52:17 +0200

    Refactor

--- kernel-design/derivatives.py
+++ kernel-design/derivatives.py
@@ -1,71 +1,109 @@
 # %%
 import numpy as np
 import jax
+
+jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 
 
-def testfun(x):
-    return np.sin(x.sum(axis=-1))
-
-
 def get_training_data(ndim, npts):
+    def testfun(x):
+        return np.sin(x.sum(axis=-1))
+
     X = np.random.uniform(0, 1, size=(npts, ndim))
     y = testfun(X)
     return X, y
 
 
-def gaussian_kernel(X, Xprime, sigma):
-    return np.exp(-np.linalg.norm((X - Xprime[:, None]), axis=-1) ** 2 / (2 * sigma**2))
+# %%
+class Kernel:
+    @staticmethod
+    def _check_shape(arr: np.ndarray, shape: tuple[int]):
+        if arr.shape != shape:
+            raise ValueError(
+                f"Shape mismatch. Please check kernel implementation. Expected {shape}, got {arr.shape}."
+            )
+
+        return arr
+
+    @staticmethod
+    def _check_args(X: np.ndarray, Xother: np.ndarray, args):
+        if len(args) != 0:
+            raise ValueError("Function requires explicit keyword-arguments.")
+
+        if Xother is None:
+            Xother = X
+
+        return X, Xother
+
+    def value(self, X: np.ndarray, Xother: np.ndarray = None, *args, **kwargs):
+        X, Xother = Kernel._check_args(X, Xother, args)
+
+        vals = np.array(self.callable(X, Xother, kwargs))
+
+        return Kernel._check_shape(vals, (X.shape[0], Xother.shape[0]))
+
+    def grad(self, X: np.ndarray, Xother: np.ndarray = None, *args, **kwargs):
+        X, Xother = Kernel._check_args(X, Xother, args)
+
+        jac = jax.jacfwd(self.callable, argnums=2)(X, Xother, kwargs)
+        jacs = []
+        for parameter in self._parameters:
+            jacs.append(np.array(jac[parameter]))
+
+        return Kernel._check_shape(
+            np.array(jacs), (len(self._parameters), X.shape[0], Xother.shape[0])
+        )
 
 
-def grad_gaussian_kernel(X, Xprime, sigma):
-    return jnp.exp(
-        -jnp.linalg.norm((X - Xprime[:, None]), axis=-1) ** 2 / (2 * sigma**2)
-    )
+class RBF(Kernel):
+    _parameters = ["sigma"]
+
+    def callable(self, X, Xother, kwargs):
+        distances = jnp.linalg.norm((Xother - X[:, None]), axis=-1)
+        return jnp.exp(-(distances**2) / (2 * kwargs["sigma"] ** 2))
 
 
-def one_run(Xtrain, ytrain, Xtest, ytest, sigma, lval):
-    npts, _ = Xtrain.shape
-    K = gaussian_kernel(Xtrain, Xtrain, sigma)
-    alpha = np.linalg.inv(K + lval * np.identity(npts)) @ ytrain
+def test_gaussian_finite_diff():
+    def one_run(kernel, Xtrain, ytrain, Xtest, ytest, sigma, lval):
+        npts, _ = Xtrain.shape
+        K = kernel.value(Xtrain, Xtrain, sigma=sigma)
+        alpha = np.linalg.inv(K.T + lval * np.identity(npts)) @ ytrain
 
-    K = gaussian_kernel(Xtrain, Xtest, sigma)
-    pred = np.sum(K * alpha, axis=1)
-    return np.average((pred - ytest) ** 2)
+        K = kernel.value(Xtrain, Xtest, sigma=sigma)
+        pred = np.sum(K.T * alpha, axis=1)
+        return np.average((pred - ytest) ** 2)
 
+    kernel = RBF()
+    ndim, npts, sigma, lval = 2, 10, 1e-1, 1e-10
 
-def value_and_grad(ndim, npts, sigma, lval):
     ntest = 100
     train = get_training_data(ndim, npts)
     test = get_training_data(ndim, ntest)
 
     # finite diff
     delta = 0.001
-    down = one_run(*train, *test, sigma + delta, lval)
-    up = one_run(*train, *test, sigma - delta, lval)
+    down = one_run(kernel, *train, *test, sigma + delta, lval)
+    up = one_run(kernel, *train, *test, sigma - delta, lval)
     deriv = (up - down) / (2 * delta)
 
     # analytical
-    K = gaussian_kernel(train[0], train[0], sigma)
+    K = kernel.value(train[0], train[0], sigma=sigma)
     beta = np.linalg.inv(K + lval * np.identity(npts))
     alpha = beta @ train[1]
 
-    gradK = np.array(
-        jax.jacfwd(grad_gaussian_kernel, argnums=2)(train[0], train[0], sigma)
-    )
+    gradK = kernel.grad(train[0], train[0], sigma=sigma)[0]
     gradbeta = -np.linalg.inv(K) @ gradK @ np.linalg.inv(K)
-    K = gaussian_kernel(train[0], test[0], sigma)
-    gradK = np.array(
-        jax.jacfwd(grad_gaussian_kernel, argnums=2)(train[0], test[0], sigma)
-    )
-    pred = np.sum(K * alpha, axis=1)
+    K = kernel.value(train[0], test[0], sigma=sigma)
+    gradK = kernel.grad(train[0], test[0], sigma=sigma)[0]
+    pred = np.sum(K.T * alpha, axis=1)
 
-    gradhat = gradbeta @ train[1] @ K.T + beta @ train[1] @ gradK.T
+    gradhat = gradbeta @ train[1] @ K + beta @ train[1] @ gradK
     deriv_analytical = -(2 * (pred - test[1]) * gradhat).sum() / ntest
 
-    return up, deriv, deriv_analytical
+    assert abs(deriv - deriv_analytical) < 1e-3
 
 
-value_and_grad(2, 10, 1e-1, 1e-10)
+test_gaussian_finite_diff()
 
 # %%
