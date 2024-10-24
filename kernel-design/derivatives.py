3b3a59f36405bd1dd1365bc71eeb570992959923
von Rudorff, Guido Falk <guido@vonrudorff.de>
2024-10-24 13:15:21 +0200

    Dataset splits

--- kernel-design/derivatives.py
+++ kernel-design/derivatives.py
@@ -1,32 +1,47 @@
 # %%
+from __future__ import annotations
 import numpy as np
 import jax
+import functools
 import time
+import ase
+
 
 jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
-from __future__ import annotations
-
-
-def get_training_data(ndim, npts):
-    def testfun(x):
-        return np.sin(x.sum(axis=-1))
-
-    X = np.random.uniform(0, 1, size=(npts, ndim))
-    y = testfun(X)
-    return X, y
 
 
 # %%
 class Kernel:
-    @staticmethod
-    def _check_shape(arr: np.ndarray, shape: tuple[int]):
-        if arr.shape != shape:
+    def _check_shape(
+        self,
+        value: np.ndarray = None,
+        gradient: np.ndarray = None,
+        training: bool = True,
+        npts: int = 1,
+    ):
+        if training:
+            len_other = npts
+        else:
+            len_other = self._dataset.ntrain - npts
+
+        gradient_shape = (len(self._parameters), npts, len_other)
+        value_shape = (npts, len_other)
+
+        if value is not None:
+            got = value.shape
+            expected = value_shape
+        if gradient is not None:
+            got = gradient.shape
+            expected = gradient_shape
+        if got != expected:
             raise ValueError(
-                f"Shape mismatch. Please check kernel implementation. Expected {shape}, got {arr.shape}."
+                f"Shape mismatch. Please check kernel implementation. Expected {expected}, got {got}."
             )
 
-        return arr
+        if value is not None:
+            return value
+        return gradient
 
     def _assert_representation(self, X: np.ndarray):
         if isinstance(X[0], ase.Atoms):
@@ -50,30 +65,58 @@ class Kernel:
 
         return X, Xother
 
-    def value(self, X: np.ndarray, Xother: np.ndarray = None, *args, **kwargs):
-        X, Xother = self._check_args(X, Xother, args, kwargs)
+    def _get_representation(
+        self, split_idx: int, npts: int, training: bool, rep_params: dict
+    ):
+        X = self._dataset.split_points(split_idx, npts, training)
+        if isinstance(X[0], ase.Atoms):
+            X = np.array([self._representation.transform(_, rep_params) for _ in X])
+        return X
 
-        vals = np.array(self.calculate_value(X, Xother, kwargs))
+    def _split_params(parameters: dict):
+        rep_params, kernel_params = {}, {}
+        for k, v in parameters.items():
+            if k.startswith("_"):
+                rep_params[k[1:]] = v
+            else:
+                kernel_params[k] = v
+        return rep_params, kernel_params
 
-        return Kernel._check_shape(vals, (X.shape[0], Xother.shape[0]))
+    def value(self, split_idx: int, npts: int, training: bool, **parameters):
+        rep_params, kernel_params = Kernel._split_params(parameters)
 
-    def grad(self, X: np.ndarray, Xother: np.ndarray = None, *args, **kwargs):
-        X, Xother = self._check_args(X, Xother, args, kwargs)
+        X = self._get_representation(split_idx, npts, True, rep_params)
+        Xother = self._get_representation(split_idx, npts, training, rep_params)
 
-        jac = jax.jacfwd(self.calculate_value, argnums=2)(X, Xother, kwargs)
+        return np.array(self.kernel_matrix(X, Xother, kernel_params))
+
+    def grad(self, split_idx: int, npts: int, training: bool, **parameters):
+        rep_params, kernel_params = Kernel._split_params(parameters)
+
+        X = self._get_representation(split_idx, npts, True, rep_params)
+        Xother = self._get_representation(split_idx, npts, training, rep_params)
+
+        jac = jax.jacfwd(self.kernel_matrix, argnums=2)(X, Xother, kernel_params)
         jacs = []
         for parameter in sorted(self._parameters):
             jacs.append(np.array(jac[parameter]))
 
-        return Kernel._check_shape(
-            np.array(jacs), (len(self._parameters), X.shape[0], Xother.shape[0])
-        )
+        return np.array(jacs)
 
     def __init__(self, representation: Representation):
         self._representation = representation
 
+        for parameter in self._parameters:
+            if parameter.startswith("_"):
+                raise ValueError("Parameters must not have leading underscores.")
+
     def prepare_for_dataset(self, dataset: list[ase.Atoms]):
-        self._representation.prepare_for_dataset(dataset)
+        self._cache = {}
+        self._dataset = dataset
+        try:
+            self._representation.prepare_for_dataset(dataset)
+        except:
+            pass
 
 
 class LinearCombination(Kernel):
@@ -85,7 +128,8 @@ class LinearCombination(Kernel):
             self._parameters.append(f"__{kernel_index}_prefactor")
             self._parameters += [f"_{kernel_index}_{_}" for _ in kernel._parameters]
 
-    def prepare_for_dataset(self, dataset: list[ase.Atoms]):
+    def prepare_for_dataset(self, dataset: Dataset):
+        super().prepare_for_dataset(dataset)
         for kernel in self._kernels:
             kernel.prepare_for_dataset(dataset)
 
@@ -97,43 +141,36 @@ class LinearCombination(Kernel):
                 subkwargs[argname] = kwargs[arg]
         return subkwargs
 
-    def value(self, X: np.ndarray, Xother: np.ndarray = None, *args, **kwargs):
-        X, Xother = self._check_args(X, Xother, args, kwargs)
-
+    def value(self, split_idx: int, npts: int, training: bool, **kwargs):
         vals = 0
         for kernel_index, kernel in enumerate(self._kernels):
             subkwargs = self._get_subkwargs(kwargs, kernel_index)
-            vals += kwargs[f"__{kernel_index}_prefactor"] * kernel.value(
-                X, Xother, *args, **subkwargs
-            )
+            vals = kernel.value(split_idx, npts, training, **subkwargs)
+            vals += kwargs[f"__{kernel_index}_prefactor"] * vals
 
-        return Kernel._check_shape(vals, (X.shape[0], Xother.shape[0]))
+        return self._check_shape(value=vals, training=training, npts=npts)
 
-    def grad(self, X: np.ndarray, Xother: np.ndarray = None, *args, **kwargs):
-        X, Xother = self._check_args(X, Xother, args, kwargs)
-
+    def grad(self, split_idx: int, npts: int, training: bool, **kwargs):
         grad = {}
         for kernel_index, kernel in enumerate(self._kernels):
             subkwargs = self._get_subkwargs(kwargs, kernel_index)
             grad[f"__{kernel_index}_prefactor"] = kernel.value(
-                X, Xother, *args, **subkwargs
+                split_idx, npts, training, **subkwargs
             )
-            kgrad = kernel.grad(X, Xother, *args, **subkwargs)
+            kgrad = kernel.grad(split_idx, npts, training, **subkwargs)
             for parameter, gradelement in zip(kernel._parameters, kgrad):
                 grad[f"_{kernel_index}_{parameter}"] = gradelement
         jacs = [grad[_] for _ in sorted(self._parameters)]
 
-        return Kernel._check_shape(
-            np.array(jacs), (len(self._parameters), X.shape[0], Xother.shape[0])
-        )
+        return self._check_shape(gradient=np.array(jacs), training=training, npts=npts)
 
 
 class RBF(Kernel):
     _parameters = ["sigma"]
 
-    def calculate_value(self, X, Xother, kwargs):
+    def kernel_matrix(self, X: np.ndarray, Xother: np.ndarray, kernel_params: dict):
         distances = jnp.linalg.norm((Xother - X[:, None]), axis=-1)
-        return jnp.exp(-(distances**2) / (2 * kwargs["sigma"] ** 2))
+        return jnp.exp(-(distances**2) / (2 * kernel_params["sigma"] ** 2))
 
 
 def test_gaussian_finite_diff():
@@ -176,14 +213,11 @@ def test_gaussian_finite_diff():
     assert abs(deriv - deriv_analytical) < 1e-3
 
 
-import ase
-
-
 class Representation:
     def prepare_for_dataset(self, dataset: list[ase.Atoms]):
         raise NotImplementedError("Representations need to implement this function.")
 
-    def transform(self, mol: ase.Atoms):
+    def transform(self, mol: ase.Atoms, parameters: dict):
         raise NotImplementedError("Representations need to implement this function.")
 
 
@@ -195,7 +229,7 @@ class CoulombMatrix(Representation):
         maxatoms = max([len(_) for _ in dataset])
         self._cm = dscribe.descriptors.CoulombMatrix(n_atoms_max=maxatoms)
 
-    def transform(self, mol: ase.Atoms):
+    def transform(self, mol: ase.Atoms, parameters: dict):
         return self._cm.create(mol)
 
 
@@ -203,98 +237,160 @@ class Identity(Representation):
     def prepare_for_dataset(self, dataset):
         pass
 
-    def transform(self, point: np.ndarray):
+    def transform(self, point: np.ndarray, parameters: dict):
         return point
 
 
-class OptKRR:
+class Dataset:
     def __init__(
-        self, kernel: Kernel, molecules: np.ndarray[ase.Atoms], labels: np.ndarray
+        self,
+        data_points: np.ndarray[ase.Atoms] | np.ndarray[float],
+        labels: np.ndarray,
+        nsplits: int,
     ):
-        self._kernel = kernel
-        self._kernel.prepare_for_dataset(molecules)
-        self._split_dataset(molecules, labels)
+        if isinstance(data_points[0], ase.Atoms):
+            self._data_points = np.array(data_points, dtype=object)
+        else:
+            self._data_points = data_points
 
-    def _split_dataset(self, points: list[ase.Atoms] | np.ndarray, labels: np.ndarray):
-        ntotal = len(points)
-        print(f"Received dataset of {ntotal} points.")
-        if isinstance(points[0], ase.Atoms):
-            points = np.array(points, dtype=object)
-        ntrain = int(0.8 * ntotal)
-        nholdout = ntotal - ntrain
-        if nholdout < 100:
-            print("WARNING: Holdout set has only few data points.")
+        self._labels = labels
+        self._split(nsplits)
 
-        train, holdout = OptKRR._random_indices(ntrain, ntotal)
-        self._training_data = (points[train], labels[train])
-        self._holdout_data = (points[holdout], labels[holdout])
+    @property
+    def npoints(self) -> int:
+        return len(self._data_points)
 
-    @staticmethod
-    def _random_indices(take: int, total: int):
-        idx = np.arange(total)
+    @property
+    def ntrain(self) -> int:
+        return int(0.8 * self.npoints)
+
+    @property
+    def nholdout(self) -> int:
+        return self.npoints - self.ntrain
+
+    @property
+    def nsplits(self) -> int:
+        return len(self._splits)
+
+    def _split(self, nsplits: int):
+        # training - holdout
+        idx = np.arange(self.npoints)
         np.random.shuffle(idx)
-        return idx[:take], idx[take:]
+        self._training_indices = idx[: self.ntrain]
+        self._holdout_indices = idx[self.ntrain :]
+
+        # splits for cross validation
+        idx = np.arange(self.ntrain)
+        self._splits = []
+        for i in range(nsplits):
+            np.random.shuffle(idx)
+            self._splits.append(idx.copy())
+
+    def _split_idx(self, split_idx, npts, training):
+        idx = self._splits[split_idx]
+        if training:
+            idx = idx[:npts]
+        else:
+            idx = idx[npts:]
+        return idx
+
+    def split_labels(self, split_idx, npts, training):
+        return self._labels[self._split_idx(split_idx, npts, training)]
+
+    def split_points(self, split_idx, npts, training):
+        return self._data_points[self._split_idx(split_idx, npts, training)]
+
+
+def get_training_data(ndim, npts):
+    def testfun(x):
+        return np.sin(x.sum(axis=-1))
+
+    X = np.random.uniform(0, 1, size=(npts, ndim))
+    y = testfun(X)
+    return Dataset(X, y, 5)
+
+
+class OptKRR:
+    def __init__(self, kernel: Kernel, dataset: Dataset):
+        self._kernel = kernel
+        self._dataset = dataset
+        self._kernel.prepare_for_dataset(dataset)
+        print(f"Received dataset of {dataset.npoints} points.")
+
+        if self._dataset.nholdout < 100:
+            print("WARNING: Holdout set has only few data points.")
 
     def _guess_parameters(self) -> dict[str, float]:
         return {_: 1.0 for _ in self._kernel._parameters}
 
-    def _get_rmse(self, train: np.ndarray, test: np.ndarray, parameters: dict):
-        Xtrain = self._training_data[0][train]
-        ytrain = self._training_data[1][train]
-        Xtest = self._training_data[0][test]
-        ytest = self._training_data[1][test]
-
-        npts = len(train)
+    def _score(self, npts: int, parameters: dict, include_grad=False):
+        avgs = []
+        derivs = []
         lval = 1e-7  # TODO
-        Ktraintrain = self._kernel.value(Xtrain, Xtrain, **parameters)
-        beta = np.linalg.inv(Ktraintrain.T + lval * np.identity(npts))
-        alpha = beta @ ytrain
-        gradKs_traintrain = self._kernel.grad(Xtrain, Xtrain, **parameters)
-        gradKs_traintest = self._kernel.grad(Xtrain, Xtest, **parameters)
-        deriv_analytical = []
-        for parameter_idx in range(gradKs_traintrain.shape[0]):
-            gradbeta = (
-                -np.linalg.inv(Ktraintrain)
-                @ gradKs_traintrain[parameter_idx]
-                @ np.linalg.inv(Ktraintrain)
-            )
+        for split_idx in range(self._dataset.nsplits):
+            ytrain = self._dataset.split_labels(split_idx, npts, True)
+            ytest = self._dataset.split_labels(split_idx, npts, False)
 
-            Ktraintest = self._kernel.value(Xtrain, Xtest, **parameters)
+            Ktraintrain = self._kernel.value(split_idx, npts, True, **parameters)
+            Ktraintrain_inv = np.linalg.inv(Ktraintrain)
+            beta = np.linalg.inv(Ktraintrain + lval * np.identity(npts))
+            alpha = beta @ ytrain
+
+            if include_grad:
+                gradKs_traintrain = self._kernel.grad(
+                    split_idx, npts, True, **parameters
+                )
+                gradKs_traintest = self._kernel.grad(
+                    split_idx, npts, False, **parameters
+                )
+            deriv_analytical = []
+            Ktraintest = self._kernel.value(split_idx, npts, False, **parameters)
             pred = np.sum(Ktraintest.T * alpha, axis=1)
-            gradhat = (
-                gradbeta @ ytrain @ Ktraintest
-                + beta @ ytrain @ gradKs_traintest[parameter_idx]
-            )
-            deriv_analytical.append(-(2 * (pred - ytest) * gradhat).sum() / len(ytest))
+            if include_grad:
+                for parameter_idx in range(gradKs_traintrain.shape[0]):
+                    gradbeta = (
+                        -Ktraintrain_inv
+                        @ gradKs_traintrain[parameter_idx]
+                        @ Ktraintrain_inv
+                    )
+                    gradhat = (
+                        gradbeta @ ytrain @ Ktraintest
+                        + alpha @ gradKs_traintest[parameter_idx]
+                    )
 
-        return np.average((pred - ytest) ** 2), np.array(deriv_analytical)
+                    deriv_analytical.append(
+                        -(2 * (pred - ytest) * gradhat).sum() / len(ytest)
+                    )
+
+            avgs.append(np.average((pred - ytest) ** 2))
+            derivs.append(np.array(deriv_analytical))
+
+        if include_grad:
+            derivs = np.median(derivs, axis=0)
+        else:
+            derivs = None
+        return np.average(avgs), derivs
 
     @property
     def ntrain(self):
         return len(self._training_data[0])
 
-    def run(self, ntrain: int, nsubsplits: int = 20):
-        if ntrain > self.ntrain:
+    def run(self, ntrain: int):
+        if ntrain > self._dataset.ntrain:
             raise ValueError(
                 f"Fewer than the requested {ntrain} data points available."
             )
 
-        subsplits = [
-            OptKRR._random_indices(ntrain, self.ntrain) for _ in range(nsubsplits)
-        ]
-
         start = self._guess_parameters()
+
         print(f"Optimizing {len(start)} parameters for the given kernel.")
         print("step | RMSE    | improvement | time | parameters")
         firstscore = None
         starttime = time.time()
         for i in range(20):
-            rmse_and_grads = [self._get_rmse(*_, start) for _ in subsplits]
-
-            score = np.average([_[0] for _ in rmse_and_grads])
+            score, grad = self._score(ntrain, start, include_grad=True)
             if firstscore is None:
                 firstscore = score
-            grad = np.median([_[1] for _ in rmse_and_grads], axis=0)
 
             print(
                 f"{i:4} | {score:5.1e} | {firstscore / score:11.1f} | {time.time()-starttime:4.1f} | {start}"
@@ -309,7 +405,7 @@ class OptKRR:
             for scale in 2.0 ** np.arange(-7, 3):
                 for pidx, parameter in enumerate(sorted(self._kernel._parameters)):
                     start[parameter] += direction[pidx] * scale
-                score = np.average([self._get_rmse(*_, start)[0] for _ in subsplits])
+                score, _ = self._score(ntrain, start, include_grad=False)
                 if best_score is None or score < best_score:
                     best_scale = scale
                     best_score = score
@@ -327,12 +423,10 @@ class OptKRR:
 # opt = OptKRR(kernel, a[:500], l[:500])
 # opt.run(199)
 if __name__ == "__main__":
-    X, y = get_training_data(3, 500)
-    # kern = LinearCombination(RBF(Identity()), RBF(Identity()))
-
-    # opt = OptKRR(RBF(Identity()), X, y)
-    opt = OptKRR(LinearCombination(RBF(Identity()), RBF(Identity())), X, y)
-    opt.run(20)
+    ds = get_training_data(3, 500)
+    kernel = RBF(Identity())
+    opt = OptKRR(kernel, ds)
+    opt.run(100)
 
 
 # %%
@@ -399,6 +493,3 @@ def database_qm9(random_limit=1000):
     energies = [energies[_] for _ in subset]
     compounds = [ase.io.read(io.StringIO(contents[_]), format="xyz") for _ in subset]
     return np.array(compounds, dtype=object), np.array(energies)
-
-
-# %%
